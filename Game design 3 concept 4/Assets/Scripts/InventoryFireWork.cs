6dcf07eead86f0938d5f22ced58e37034a795838
Faethryn <laurens.muylle@student.howest.be>
2022-04-19 13:50:22 +0200

    opened and set up some basics for the firework

--- Game design 3 concept 4/Assets/Scripts/InventoryFireWork.cs
+++ Game design 3 concept 4/Assets/Scripts/InventoryFireWork.cs
@@ -6,10 +6,18 @@ public class InventoryFireWork : MonoBehaviour
 {
     [SerializeField]
     private GameObject _FireworkObject;
+    [SerializeField]
+    private Transform _placementTransform;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire1"))
+        {
+           
+             
+                Instantiate(_FireworkObject, _placementTransform.position, _placementTransform.rotation);
+            
+        }
     }
 }
