3b4e3cd3bad89c9a24b6d3da33d983c41da3b3df
Faethryn <laurens.muylle@student.howest.be>
2022-04-25 17:22:58 +0200

    made match spawning possible

--- Game design 3 concept 4/Assets/Scripts/InventoryMatch.cs
+++ Game design 3 concept 4/Assets/Scripts/InventoryMatch.cs
@@ -6,10 +6,22 @@ public class InventoryMatch : MonoBehaviour
 {
     [SerializeField]
     private GameObject _ThrowingMatchPrefab;
+    [SerializeField]
+    private Transform _spawnTransform;
+
+    [SerializeField]
+    private int _startingAmount;
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (Input.GetButtonDown("Fire1") && _startingAmount > 0)
+        {
+
+            Instantiate(_ThrowingMatchPrefab, _spawnTransform.position, _spawnTransform.rotation);
+            _startingAmount -= 1;
+        }
+
     }
 }
